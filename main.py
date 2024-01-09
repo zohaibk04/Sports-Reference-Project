77bf09c934de3dc2581341dac6c0ce21184d8c72
zohaibk4 <zohaibk1204@gmail.com>
2024-01-08 21:42:46 -0500

    Completed code to display the table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import dtale as dt
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 import json
 import sys
 
@@ -38,4 +39,36 @@ for team1 in tm_list:
     table[team1] = losses
 df = pd.DataFrame(table)
 df.loc[len(df)] = df.columns
-print(df)
+
+# Setting up the figure and axis to display the table
+figure, axis = plt.subplots()
+figure.patch.set_visible(False)
+axis.axis('off')
+axis.axis('tight')
+
+# Getting the column header/cell colors depending on the row
+cell_colors = [['white' for _ in range(len(df.columns))]] * len(df)
+col_colors = ['#eeeeee' for _ in range(len(df.columns))]
+for i in range(len(df)):
+    if i in range(len(df)-1):
+        cell_colors[i] = ['white' for _ in df.iloc[i]]
+    else:
+        cell_colors[i] = ['#eeeeee' for _ in df.iloc[i]]
+
+# Creating the table and configuring the text style based on row
+table = axis.table(cellText=df.values, cellLoc='center', colLabels=df.columns, cellColours=cell_colors, loc='center', colColours=col_colors)
+for (row, col), cell in table.get_celld().items():
+    if (row == 0):
+        cell.set_text_props(fontproperties=FontProperties(weight='bold'))
+        cell.get_text().set_color('#990609')
+    elif (row == len(df)):
+        cell.set_text_props(fontproperties=FontProperties(weight='bold'))
+
+# Used to make the table size correspond to cell content
+table.auto_set_font_size(False)
+table.set_fontsize(10)
+table.auto_set_column_width(col=list(range(len(df.columns))))
+
+# Displaying the table in a popup window
+figure.tight_layout()
+plt.show()
